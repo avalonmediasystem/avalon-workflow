7c31b3a38aab2c2e3ed03b425b5f2b6ca6d17b3d
Nathan Rogers <rogersna@indiana.edu>
2012-11-07 16:27:47 -0500

    Moving implemented steps to app/

---
+++ app/models/file_upload_step.rb
@@ -0,0 +1,32 @@
+require 'hydrant/dropbox'
+require 'hydrant/workflow'
+
+module Hydrant
+	module Workflow
+		class FileUploadStep < BasicStep
+		# For file uploads the process of setting the context is easy. We
+		# just need to ask the dropbox if there are any files. If so load
+		# them into a variable that can be referred to later
+		def before_step context
+	       dropbox_files = Hydrant::DropboxService.all
+		   context[:dropbox_files] = @dropbox_files 
+		   context
+		end
+
+		def after_step
+		end
+
+		def execute context
+	       logger.debug "<< Processing FILE-UPLOAD step >>"
+		   update_master_files context[:mediaobject], context[:parts] 
+		   unless context[:mediaobject].parts.empty?
+		       media = context[:mediaobject]
+			   media.format = media.parts.first.media_type
+			   media.save(validate: false)
+			   context[:mediaobject] = media
+		   end
+
+		   context
+		end
+	end
+end
